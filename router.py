2694843c1d4340c4b124087da2fc7bf92c1ad1cd
g-soares <gabrieldpr_@hotmail.com>
2018-10-28 19:12:40 -0300

    finalizado mas não testado

--- router.py
+++ router.py
@@ -5,6 +5,7 @@ import select
 import time
 import json
 import os
+from struct import pack, unpack
 
 TAM_MAX_PACOTE = 39+ (2**14)
 
@@ -12,8 +13,15 @@ class Rota():
 	def __init__(self, destino, caminho, peso):
 		self.destino = destino
 		self.caminho = caminho
-		self.peso = peso
+		self.peso = int(peso)
 		self.timeStamp = time.time()
+
+	def __str__(self):
+		return f'''destino: {self.destino}
+		\rcaminho: {self.caminho}
+		\rpeso: {self.peso}
+		\rtime stamp: {self.timeStamp}
+		'''
 
 class Router:
 	def __init__(self):
@@ -43,38 +51,56 @@ class Router:
 		try:
 			for linha in open(nomeArquivo,'r'):
 				comando, ip, peso = linha.split(' ')
-				if not (ip in self.mapa):
-					self.mapa[ip] = []
 				self.adicionarDados(Rota(ip, ip, peso))
 		except :
 			print(f'Falha ao abrir o arquivo de startup-commands {nomeArquivo}')
 
-	
 	#adiciona dados ao vetor de distâncias
 	def adicionarDados(self, rota):
-		lista = mapa[rota.destino]
+		if rota.destino not in self.mapa:
+			self.mapa[rota.destino] = []
 
-		with permissaoMapa:
-			if not lista: #testa lista vazia
-				lista.appen(rota)
-				self.iniciaTemporizador(rota.destino, rota.caminho)
+		lista = self.mapa[rota.destino]
 
-			elif (lista[0].peso == rota.peso and 
-				lista[0].caminho is not rota.caminho): #testa se é uma rota alternativa
+		with self.permissaoMapa:
+			#testa lista vazia
+			if not lista:
+				#print('rota adicionada')
 				lista.append(rota)
 				self.iniciaTemporizador(rota.destino, rota.caminho)
 
-			elif lista[0].peso > rota.peso: #testa se é uma rota melhor
+			#testa se é uma rota alternativa ou a uma rota que já é conhecida
+			elif lista[0].peso == rota.peso:
+				existe = False
+
+				for aux in lista:
+					if aux.caminho == rota.caminho:
+						#print('timeStamp atualizado')
+						aux.timeStamp = rota.timeStamp
+						existe = True
+
+				if not existe :
+					#print('rota alternativa')
+					lista.append(rota)
+					self.iniciaTemporizador(rota.destino, rota.caminho)
+
+			#testa se é uma rota melhor
+			elif lista[0].peso > rota.peso:
+				#print('rota melhor')
 				del lista[:]
 				lista.append(rota)
 				self.iniciaTemporizador(rota.destino, rota.caminho)
 
+			#testa se a rota piorou
 			elif (len(lista) == 1 and 
 				lista[0].peso < rota.peso and 
-				lista[0].caminho == rota.caminho): #testa se a rota piorou
+				lista[0].caminho == rota.caminho):
+				#print('rota pior')
 				lista[0] = rota
 
-			elif len(lista) > 1: #testa se a rota piorou
+			#testa se a rota piorou
+			elif len(lista) > 1:
+				#print('uma das rotas piorou')
 				for count, dados in enumerate(lista):
 					if dados.caminho == rota.caminho and dados.peso < rota.peso:
 						del lista[count]
@@ -90,38 +116,81 @@ class Router:
 
 		while existeRota:
 			existeRota = False
-			for rota in mapa[destino]:
-				if (rota.destino == destino and 
-					rota.caminho == caminho and 
-				   (time.time() - rota.timeStamp) < 4*self.period ):
-					existeRota = True
+			rotas = None
 
-			time.sleep(4*self.period)
+			with self.permissaoMapa:
+				if destino in self.mapa:
+					rotas = self.mapa[destino].copy()
+			
+			if rotas:
+				for rota in rotas:
+					if (rota.destino == destino and 
+						rota.caminho == caminho and 
+					   (time.time() - rota.timeStamp) < 4*self.period ):
+						existeRota = True
+
+				time.sleep(4*self.period)
 
 		self.removerDados(destino, caminho)
 
 	def removerDados(self, destino, caminho):
-		with permissaoMapa:
-			for count, rota in enumerate(mapa[destino]):
-				if rota.caminho == caminho:
-					del mapa[destino][count]
-					break
+		if destino in self.mapa:
+			with self.permissaoMapa:
+				for count, rota in enumerate(self.mapa[destino]):
+					if rota.caminho == caminho:
+						del self.mapa[destino][count]
+						break
 
-		if not mapa[destino]:
-			self.removerLink(destino)
+			if not self.mapa[destino]:
+				self.removerLink(destino)
 
 	def removerLink(self, ip):
-		with permissaoMapa:
-			mapa.pop(ip)
+		with self.permissaoMapa:
+			self.mapa.pop(ip)
 
 	def enviarTrace(self, destino):
-		pass
+		pacote = {"type": "trace"}
+		pacote["source"] = self.HOST
+		pacote["destination"] = destino
+		pacote["hops"] = []
+
+		self.encaminharPacote(pacote)
 
 	def encaminharPacote(self, pacote):
-		pass
+		#print(f'encaminhar pacote -> {pacote}')
+		if pacote["type"] == "trace":
+			pacote["hops"].append(f"{self.HOST}")
 
-	def tratarPacote():
-		pass
+		pacoteEnviado = json.dumps(pacote)
+		endereco = pacote["destination"]
+		self.sock.sendto(pack('!{}s'.format(len(pacoteEnviado)),pacoteEnviado.encode())
+			, (endereco, self.PORT))
+
+		#altera a ordem da lista para fazer o balanceamento de carga
+		if len(self.mapa[endereco]) > 1:
+			with self.permissaoMapa:
+				self.mapa[endereco] = self.mapa[endereco][1:].append(self.mapa[endereco][0])
+
+	def tratarPacote(self, pacote):
+		print(f'Tratar pacote -> {pacote}')
+		if pacote["type"] == "data":
+			print(pacote["payload"])
+
+		elif pacote["type"] == "update":
+			pesoAteVizinho = self.mapa[pacote["source"]][0].peso
+
+			for dado in pacote["distances"]:
+				self.adicionarDados(Rota(dado, pacote["source"], pacote["distances"][dado]+ pesoAteVizinho))
+		
+		elif pacote["type"] == "trace":
+			pacote["hops"].append(self.HOST)
+			novoPacote = {}
+			novoPacote["type"] = "data"
+			novoPacote["source"] = self.HOST
+			novoPacote["destination"] = pacote["destination"]
+			novoPacote["payload"] = pacote
+
+			self.encaminharPacote(novoPacote)
 
 	def rotearPacotes(self):
 		while self.ligado:
@@ -142,23 +211,28 @@ class Router:
 			time.sleep(self.period)
 			dados = None
 
+			print('---------------')
+			print(self.mapa)
+			print('---------------')
+
 			with self.permissaoMapa:
 				dados = self.mapa.copy()
 			
 			for endereco in dados:
 				if dados[endereco][0].destino == dados[endereco][0].caminho:
 					pacote = {"type": "update"}
-					pacote["source"] = f'"{self.HOST}"'
-					destination = {}
+					pacote["source"] = self.HOST
+					pacote["destination"] = endereco
+					distances = {}
 					
 					for auxEndereco in dados:
 						#split horizon
 						if not (dados[auxEndereco][0].caminho == endereco):
-							destination[f'"{dados[auxEndereco][0].endereco}"'] = dados[auxEndereco][0].peso
+							distances[dados[auxEndereco][0].endereco] = dados[auxEndereco][0].peso
 
-					destination[f'"{self.HOST}"'] = 0
-					pacote["destination"] = distances
-					self.sock.sendto(json.dumps(pacote), (self.host, self.port))
+					distances[self.HOST] = 0
+					pacote["distances"] = distances
+					self.encaminharPacote(pacote)
 
 	def __str__(self):
 		return f'''PORT: {self.PORT}
@@ -202,7 +276,7 @@ if __name__ == '__main__':
 			if entrada[0] == 'add':
 				roteador.adicionarDados(Rota(entrada[1],entrada[1],entrada[2]))
 			elif entrada[0] == 'del':
-				roteador.removerDados(Rota(entrada[1],entrada[1],entrada[2]))
+				roteador.removerLink(entrada[1])
 			elif entrada[0] == 'trace':
 				roteador.enviarTrace(entrada[1])
 			elif entrada [0] == 'quit':
